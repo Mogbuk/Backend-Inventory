0ed6d35d4fcf58e7920ea2e3274e832af3e784b9
Mogbuk <xsebastianrojasx@gmail.com>
2025-10-13 23:35:55 -0500

    Configuracion TypeORM con la conexion

---
+++ src/config/datasource.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true, 
+  logging: true,
+  entities: [__dirname + "/../entities/*.ts"],
+});
