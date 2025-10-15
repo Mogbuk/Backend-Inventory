6af418763a704353c447366f06e5fd2dea0c7226
Mogbuk <xsebastianrojasx@gmail.com>
2025-10-14 21:22:44 -0500

    Se importó las entidades y añadidas al datasource

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -1,5 +1,10 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { Company } from "../entities/Company";
+import { Warehouse } from "../entities/Warehouse";
+import { Product } from "../entities/Product";
+import { Stock } from "../entities/Stock";
+import { Movement } from "../entities/Movement";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,10 +12,10 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  synchronize: true, 
-  logging: true,
-  entities: [__dirname + "/../entities/*.ts"],
   ssl: {
     rejectUnauthorized: false,
   },
+  synchronize: true,
+  logging: true,
+  entities: [Company, Warehouse, Product, Stock, Movement],
 });
