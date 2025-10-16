a585de9e06c649e77240d0af5747755e8cab3150
Mogbuk <xsebastianrojasx@gmail.com>
2025-10-15 21:28:23 -0500

    Se implementó las rutas de stock

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./controllers/products.controller";
+import stockRoutes from "./controllers/stock.controller";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/stock", stockRoutes);
+
 
 export default app;
