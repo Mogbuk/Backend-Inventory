24b28c4ab15c2c6baaf1ab4c08037feddfd7ad12
Mogbuk <xsebastianrojasx@gmail.com>
2025-10-19 16:46:19 -0500

    Fix package.json and app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,6 @@ import movementRoutes from "./controllers/movement.controller";
 import companyRoutes from "./controllers/companies.controller";
 import { errorHandler } from "./middlewares/errorHandler";
 
-const swaggerFilePath = path.join(__dirname, "../swagger.json");
-const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, "utf8"));
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -26,7 +22,6 @@ app.use("/api/companies", companyRoutes);
 app.use("/api/warehouses", warehouseRoutes);
 app.use("/api/movements", movementRoutes);
 app.use("/api", stockRoutes);
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
